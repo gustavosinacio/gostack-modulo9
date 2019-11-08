428c36d1a6fae667e45d54849d8fcf2ff86a0c52
Gustavo <gsinacio94@gmail.com>
2019-11-08 10:45:25 -0300

    minor

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,7 +22,7 @@ export function* signIn({ payload }) {
 
   yield put(signInSuccess(token, user));
 
-  history.pushState('/dashboard');
+  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
